e3f06e6df8b7d26c0717790b1fd1d328bd4125ca
Dario <filatrella.dario@gmail.com>
2020-06-21 16:52:59 +0200

    Migrated user model

--- users/admin.py
+++ users/admin.py
@@ -5,8 +5,13 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User
-# class CustomUserAdmin(UserAdmin):
-#     model = CustomUser
-#     list_display = ['email', 'username']
-#
-admin.site.register(User)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'last_login')
+    list_filter = ('last_login', 'is_superuser', 'is_staff')
+    ordering = ('id',)
+    search_fields = ('username',)
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -2,16 +2,52 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin, UserManager, UnicodeUsernameValidator
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 from rest_framework.authtoken.models import Token
 from django.utils.translation import gettext_lazy as _
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         db_table = 'auth_user'
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    # test_field = models.BooleanField(null=False, default=False)
 
 
 
