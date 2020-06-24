e21249efb7cb61e2b486c0f043af8d66fbc2ae0d
Dario <filatrella.dario@gmail.com>
2020-06-24 14:57:15 +0200

    forse a sto giro va

--- BudgetApp/settings.py
+++ BudgetApp/settings.py
@@ -191,16 +191,6 @@ DATABASES = {
 
 }
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "asgi_redis.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-#         },
-#         "ROUTING": "chat.routing.channel_routing",
-#     },
-# }
-
 CHANNEL_LAYERS = {
     'default': {
         "BACKEND": "asgi_redis.RedisChannelLayer",
