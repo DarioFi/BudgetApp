fd42771313393acefa4b42ad303279827ad6e992
Dario <filatrella.dario@gmail.com>
2020-06-24 15:47:39 +0200

    uso channels_redis invece di asgiredis

--- BudgetApp/settings.py
+++ BudgetApp/settings.py
@@ -193,7 +193,7 @@ DATABASES = {
 
 CHANNEL_LAYERS = {
     'default': {
-        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
         },
